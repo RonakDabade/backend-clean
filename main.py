17a82db9a1066d9447f9f633d80a5ca471cc2718
Ronak Ramchandra Dabade <Ronak@Ronaks-MBP.bwrouter>
2026-01-19 08:51:17 +0530

    Added frontend Link

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
+    allow_orignis=["https://frontend-flax-alpha-40.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
